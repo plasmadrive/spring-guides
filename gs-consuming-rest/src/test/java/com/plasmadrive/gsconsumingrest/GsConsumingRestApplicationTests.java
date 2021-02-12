ba3de42cd77693cc9f2a3449e543042d7fd0c33d
Gareth <gareth.badenhorst@gmail.com>
2021-02-12 08:57:51 +0000

    Commit gs-consuming-rest

---
+++ gs-consuming-rest/src/test/java/com/plasmadrive/gsconsumingrest/GsConsumingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasmadrive.gsconsumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsConsumingRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
