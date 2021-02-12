d0f37c6e7f1696238b53184aa82af3037582f577
Gareth <gareth.badenhorst@gmail.com>
2021-02-12 14:07:45 +0000

    gs-rest-hateoas

---
+++ gs-rest-hateoas/src/main/java/com/plasmadrive/gsresthateoas/GsRestHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.plasmadrive.gsresthateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRestHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsRestHateoasApplication.class, args);
+    }
+
+}
---
+++ gs-rest-hateoas/src/test/java/com/plasmadrive/gsresthateoas/GsRestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasmadrive.gsresthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
