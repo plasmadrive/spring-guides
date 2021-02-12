fd1e6761a1581d11eb12720a25b0ce098f166eaf
Gareth <gareth.badenhorst@gmail.com>
2021-02-12 15:47:58 +0000

    gs-securing-web

---
+++ gs-securing-web/src/main/java/com/plasmadrive/gssecuringweb/GsSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.plasmadrive.gssecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsSecuringWebApplication.class, args);
+    }
+
+}
---
+++ gs-securing-web/src/test/java/com/plasmadrive/gssecuringweb/GsSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasmadrive.gssecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
